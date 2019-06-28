16ccfe5f98ec63ffd17adb8919e8dd34a685ab6b
Ozuna <oppaozuna@gmail.com>
2019-06-28 15:13:32 -0500

    codebar

--- components/dashboard/views/entrada/NewEntrada.js
+++ components/dashboard/views/entrada/NewEntrada.js
@@ -1,8 +1,11 @@
-import React, {Component, useState, useContext} from 'react';
-import { Text, View, Alert } from 'react-native';
-import { ThemeProvider, Input, Button } from 'react-native-elements';
+import React, {Component, useState, useContext, useEffect} from 'react';
+import { View, Alert, TouchableOpacity, Modal } from 'react-native';
+import { ThemeProvider, Input, Button, Text } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {UserContext} from '../../../../UserContext';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 
 
@@ -10,6 +13,16 @@ NewEntrada= () =>{
 
 	const [user,setAuth,setLog,axi]=useContext(UserContext);
 	const [entrada, setEntrada] = useState({});
+	const [permiso, setPermiso] = useState(null);
+	const [modalVisible,setModalVisible]=useState(false)
+
+	useEffect(()=>{
+		async function fetchData() {
+			const { status } = await Permissions.askAsync(Permissions.CAMERA);
+			setPermiso(status === 'granted')
+		}
+		fetchData()
+	},[])
 
 	const register=()=>{
 		axi.post('/api/auth/entrada',entrada)
@@ -24,10 +37,30 @@ NewEntrada= () =>{
 			Alert.alert("Error","Se a producido un error porfavor verifique sus datos y vuelva a intentarlo")
 		})
 	}
+
+	_handleBarCodeRead = data => {
+		setEntrada({...entrada,codigo:data.data})
+		setModalVisible(false)
+	};
+
 	return (
 		<ThemeProvider theme={{ colors: {primary: 'black'}}}>
+			<Modal animationType="slide" transparent={false} visible={modalVisible}onRequestClose={() => {setModalVisible(false)}}>
+				<View style={{flex: 1,alignItems: 'center',justifyContent: 'center',paddingTop: Constants.statusBarHeight,backgroundColor: '#ecf0f1',}}>
+					{permiso === null ?
+						<Text>Solicitando permiso de la camara</Text> :
+						permiso === false ?
+						<Text>Permiso de camara rechazado</Text> :
+						<View style={{alignItems:'center',justifyContent:'center'}}>
+							<Text>Escaneando</Text>
+							<BarCodeScanner onBarCodeScanned={_handleBarCodeRead} style={{ height: 400, width: 400 }}/>
+						</View>
+					}
+				</View>
+			</Modal>
 			<View style={{padding:20}}>
 				<Input label="Codigo" onChangeText={(t)=>setEntrada({...entrada, codigo:t})} value={entrada.codigo} placeholder='Codigo de producto' leftIcon={<Icon name='barcode' size={24}/>}/>
+				<Button onPress={()=>{setModalVisible(true)}} title="Scanear" type="clear"/>
 				<Input label="Cantidad" onChangeText={(t)=>setEntrada({...entrada, cantidad:t})} value={entrada.cantidad} placeholder='Cantidad' leftIcon={<Icon name='shopping-cart' size={24}/>}/>
 				<Input label="Factura" onChangeText={(t)=>setEntrada({...entrada, ref_factura:t})} value={entrada.ref_factura} placeholder='Referencia de factura' leftIcon={<Icon name='file' size={24} color='black'/>}/>
 				<View style={{marginTop:20}}>
--- components/dashboard/views/producto/NewProducto.js
+++ components/dashboard/views/producto/NewProducto.js
@@ -1,14 +1,20 @@
 import React, {Component, useState, useContext, useEffect} from 'react';
-import { Text, View, StyleSheet, Alert, Picker, ScrollView, ActivityIndicator } from 'react-native';
+import { Text, View, StyleSheet, Alert, Picker, ScrollView, ActivityIndicator, Modal } from 'react-native';
 import { ThemeProvider, Input, Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {UserContext} from '../../../../UserContext';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 NewProducto = () =>{
+
 	const [user,setAuth,setLog,axi]=useContext(UserContext);
 	const [producto, setProducto]=useState({})
 	const [proveedores,setProv]=useState([])
 	const [loader,setLoader]=useState(true)
+	const [permiso, setPermiso] = useState(null);
+	const [modalVisible,setModalVisible]=useState(false)
 
 	useEffect(()=>{
 		axi.get('/api/auth/proveedor')
@@ -16,7 +22,13 @@ NewProducto = () =>{
 			setProv(response.data)
 			setLoader(false)
 		})
+		async function fetchData() {
+			const { status } = await Permissions.askAsync(Permissions.CAMERA);
+			setPermiso(status === 'granted')
+		}
+		fetchData()
 	},[])
+
 	const register=()=>{
 		axi.post('/api/auth/producto',producto)
 		.then((response)=>{
@@ -28,6 +40,11 @@ NewProducto = () =>{
 		})
 	}
 
+	_handleBarCodeRead = data => {
+		setProducto({...producto,codigo:data.data})
+		setModalVisible(false)
+	};
+
 	if(loader)
 		return (
 			<View style={{flex: 1,justifyContent: 'center'}}>
@@ -37,9 +54,23 @@ NewProducto = () =>{
 
 	return (
 		<ThemeProvider theme={{ colors: {primary: 'black'}}}>
+			<Modal animationType="slide" transparent={false} visible={modalVisible}onRequestClose={() => {setModalVisible(false)}}>
+				<View style={{flex: 1,alignItems: 'center',justifyContent: 'center',paddingTop: Constants.statusBarHeight,backgroundColor: '#ecf0f1',}}>
+					{permiso === null ?
+						<Text>Solicitando permiso de la camara</Text> :
+						permiso === false ?
+						<Text>Permiso de camara rechazado</Text> :
+						<View style={{alignItems:'center',justifyContent:'center'}}>
+							<Text>Escaneando</Text>
+							<BarCodeScanner onBarCodeScanned={_handleBarCodeRead} style={{ height: 400, width: 400 }}/>
+						</View>
+					}
+				</View>
+			</Modal>
 			<View style={{padding:20}}>
 				<Input label="Nombre" onChangeText={(t)=>setProducto({...producto,nombre:t})} value={producto.nombre} placeholder='Nombre del Producto' leftIcon={<Icon name='cube' size={24}/>}/>
 				<Input label="Codigo" onChangeText={(t)=>setProducto({...producto,codigo:t})} value={producto.codigo} placeholder='Codigo' leftIcon={<Icon name='barcode' size={24}/>}/>
+				<Button onPress={()=>{setModalVisible(true)}} title="Scanear" type="clear"/>
 				<Input label="Precio" onChangeText={(t)=>setProducto({...producto,precio:t})} value={producto.precio} placeholder='Precio' leftIcon={<Icon name='dollar' size={24}/>}/>
 				<Input label="Descripcion" onChangeText={(t)=>setProducto({...producto,descripcion:t})} value={producto.descripcion} placeholder='Descripcion' leftIcon={<Icon name='clipboard' size={24}/>}/>
 				<ScrollView>
